3a160face007c3fc072818de5da781c28b64a546
David Gonçalves <gabifortnite2008@gmail.com>
2025-12-12 15:05:23 -0100

    iris stuff
    run flask & test in postman

---
+++ iris-flower/irisreader.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+model_path = r'C:\Users\4539\Documents\GitHub\A.I-project\iris-flower\iris_model.pkl'
+scaler_path = r'C:\Users\4539\Documents\GitHub\A.I-project\iris-flower\scaler.pkl'
+
+model = joblib.load(model_path)
+scaler = joblib.load(scaler_path)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json  # Espera JSON: {"features": [5.9, 3.0, 5.1, 1.8]}
+    features = np.array(data['features']).reshape(1, -1)
+    scaled = scaler.transform(features)
+    pred = model.predict(scaled)[0]
+    species = {0: 'Iris-setosa', 1: 'Iris-versicolor', 2: 'Iris-virginica'}[pred]
+    return jsonify({'species': species})
+
+if __name__ == '__main__':
+    app.run(debug=True)
