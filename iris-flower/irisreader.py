609939685e4478b3d56bb66ebb68ed130b0630a2
Leonor Oliveira <gabifortnite2008@gmail.com>
2025-12-16 10:37:58 -0100

    fixed iris
    pipelines and no scalers

--- iris-flower/irisreader.py
+++ iris-flower/irisreader.py
@@ -3,20 +3,23 @@ import joblib
 import numpy as np
 
 app = Flask(__name__)
-model_path = r'C:\Users\4539\Documents\GitHub\A.I-project\iris-flower\iris_model.pkl'
-scaler_path = r'C:\Users\4539\Documents\GitHub\A.I-project\iris-flower\scaler.pkl'
 
-model = joblib.load(model_path)
-scaler = joblib.load(scaler_path)
+pipeline = joblib.load('iris-flower\iris_pipeline.pkl')
+
+species_map = {
+    0: 'Iris-setosa',
+    1: 'Iris-versicolor',
+    2: 'Iris-virginica'
+}
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.json  # Espera JSON: {"features": [5.9, 3.0, 5.1, 1.8]}
+    data = request.json  # {"features": [5.9, 3.0, 5.1, 1.8]}
     features = np.array(data['features']).reshape(1, -1)
-    scaled = scaler.transform(features)
-    pred = model.predict(scaled)[0]
-    species = {0: 'Iris-setosa', 1: 'Iris-versicolor', 2: 'Iris-virginica'}[pred]
-    return jsonify({'species': species})
+
+    pred = pipeline.predict(features)[0]
+
+    return jsonify({'species': species_map[pred]})
 
 if __name__ == '__main__':
     app.run(debug=True)
